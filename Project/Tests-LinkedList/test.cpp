1fb3b172fca9bccd04fe1a0f7995c755dd62a34b
nicollasprado <nicollasprado.profissional@gmail.com>
2024-12-27 17:35:07 -0300

    Finalização da classe de linked list

--- Project/Tests-LinkedList/test.cpp
+++ Project/Tests-LinkedList/test.cpp
@@ -5,7 +5,54 @@ int main(){
     LinkedList ll;
 
     ll.push_back(3);
-    ll[0] = 4;
+    ll.push_back(5);
+    ll[1] = 4;
+    ll.push_front(100);
+    ll.push_back(50);
+    ll.push_back(3);
+    ll.push_back(4);
+    ll.push_back(29);
 
-    std::cout << ll[0] << std::endl;
+    std::cout << "size: " << ll.size() << " find first: " << ll.find_first(3) << std::endl;
+    std::cout << "count: " << ll.count(3) << " Sum: " << ll.sum() << std::endl;
+
+    for(int i = 0; i < ll.size(); i++){
+        std::cout << ll[i] << std::endl;
+    }
+
+    std::cout << "Find all: " << std::endl;
+    LinkedList find_all_indexes = ll.find_all(3);
+
+    for(int i = 0; i < find_all_indexes.size(); i++){
+        std::cout << find_all_indexes[i] << std::endl;
+    }
+
+
+    ll.pop_back();
+    std::cout << "size after pop_back: " << ll.size() << std::endl;
+    for(int i = 0; i < ll.size(); i++){
+        std::cout << ll[i] << std::endl;
+    }
+
+    ll.pop_front();
+    std::cout << "size after pop_front: " << ll.size() << std::endl;
+    for(int i = 0; i < ll.size(); i++){
+        std::cout << ll[i] << std::endl;
+    }
+
+    ll.remove_first(4);
+    std::cout << "size after remove_first: " << ll.size() << std::endl;
+    for(int i = 0; i < ll.size(); i++){
+        std::cout << ll[i] << std::endl;
+    }
+
+    ll.remove_at(2);
+    std::cout << "size after remove_at: " << ll.size() << std::endl;
+    for(int i = 0; i < ll.size(); i++){
+        std::cout << ll[i] << std::endl;
+    }
+
+    ll.clear();
+    std::cout << "size after clear: " << ll.size() << std::endl;
+
 }
--- Project/linkedlist.hpp
+++ Project/linkedlist.hpp
@@ -2,8 +2,6 @@
 #define __LINKED_LIST_IFRN__
 
 #include <stdexcept>
-#include <string>
-
 
 class LinkedList {
     private:
@@ -23,11 +21,35 @@ class LinkedList {
         }
 
         ~LinkedList() {
+            for(unsigned int i = 0; i < size_; i++){
+                if(i == (size_ - 1)){
+                    delete tail;
+                    head = nullptr;
+                    tail = nullptr;
+                    size_ = 0;
+                }else{
+                    tail = tail->prev;
+                    delete tail->next;
+                }
+            }
         }
 
 
 
         // Get operations
+
+
+        int size(){
+            return size_;
+        }
+
+        int front(){
+            return head->value;
+        }
+
+        int back() {
+            return tail->value;
+        }
 
 
         int& operator[](int index){
@@ -42,13 +64,104 @@ class LinkedList {
                     }
                 }
 
+
                 int_node* iter = head;
-                for(int i = 0; i <= index; i++){
+                for(int i = 0; i < index; i++){
                     iter = iter->next;
                 }
 
                 return iter->value;
             }
+        }
+
+        int find_first(int value){
+            int found = -1;
+
+            int_node* iter = head;
+            for(unsigned int i = 0; i < (size_ - 1); i++){
+                if(iter->value == value){
+                    found = i;
+                    break;
+                }
+
+                iter = iter->next;
+            }
+
+            return found;
+        }
+
+        LinkedList find_all(int value){
+            LinkedList found_index_list;
+
+            unsigned int qt_iterations = (this->size_) / 2;
+
+            if(qt_iterations % 2 != 0){
+                qt_iterations++;
+            }
+
+            int_node* start = this->head;
+            int_node* end = this->tail;
+            for(unsigned int i = 0; i <= qt_iterations; i++){
+                if(end->value == value){
+                    unsigned int val_index = (this->size_) - i;
+                    found_index_list.push_back(val_index);
+                }
+
+                if(start == end){
+                    continue;
+                }
+
+                if(start->value == value){
+                    found_index_list.push_back(i);
+                }
+
+                start = start->next;
+                end = end->prev;
+            }
+
+            return found_index_list;
+        }
+
+        int count(int value){
+            int val_count = 0;
+
+            unsigned int qt_iterations = size_ / 2;
+            if(qt_iterations % 2 != 0){
+                qt_iterations++;
+            }
+
+            int_node* start = head;
+            int_node* end = tail;
+            for(unsigned int i = 0; i <= qt_iterations; i++){
+                if(end->value == value){
+                    val_count++;
+                }
+
+                if(start == end){
+                    continue;
+                }
+
+                if(start->value == value){
+                    val_count++;
+                }
+
+                start = start->next;
+                end = end->prev;
+            }
+
+            return val_count;
+        }
+
+        int sum(){
+            int total_sum = 0;
+
+            int_node* iter = head;
+            for(unsigned int i = 0; i < size_; i++){
+                total_sum += iter->value;
+                iter = iter->next;
+            }
+
+            return total_sum;
         }
 
 
@@ -60,29 +173,232 @@ class LinkedList {
         void push_back(int value){
             if(size_ == 0){
                 // creation of the new node
-                int_node new_val;
-                new_val.value = value;
-                new_val.next = nullptr;
-                new_val.prev = nullptr;
+                int_node* new_val = new int_node;
+                new_val->value = value;
+                new_val->next = nullptr;
+                new_val->prev = nullptr;
 
                 // updating the linked list
-                head = &new_val;
-                tail = &new_val;
-
-                size_++;
+                head = new_val;
+                tail = new_val;
             }else{
-                int_node new_val;
-                new_val.value = value;
-                new_val.next = nullptr;
+                int_node* new_val = new int_node;
+                new_val->value = value;
+                new_val->next = nullptr;
+                new_val->prev = tail;
 
                 // change the tail node next to the new value
-                tail->next = &new_val;
+                tail->next = new_val;
 
                 // updates tail pointer to new node
-                tail = &new_val;
-
-                size_++;
+                tail = new_val;
             }
+
+            size_++;
+        }
+
+        void push_front(int value){
+            if(size_ == 0){
+                int_node* new_val = new int_node;
+                new_val->value = value;
+                new_val->next = nullptr;
+                new_val->prev = nullptr;
+
+                head = new_val;
+                tail = new_val;
+            }else{
+                int_node* new_val = new int_node;
+                new_val->value = value;
+                new_val->next = head;
+                new_val->prev = nullptr;
+
+                head->prev = new_val;
+
+                head = new_val;
+            }
+
+            size_++;
+        }
+
+
+
+
+        // Remove operations
+
+
+        void clear(){
+            for(unsigned int i = 0; i < size_; i++){
+                if(i == (size_ - 1)){
+                    delete tail;
+                    head = nullptr;
+                    tail = nullptr;
+                    size_ = 0;
+                }else{
+                    tail = tail->prev;
+                    delete tail->next;
+                }
+            }
+        }
+
+        bool pop_back(){
+            bool deleted = false;
+
+            if(size_ == 0){
+                throw std::runtime_error("Error trying to pop_back -- The list is empty");
+            }else if(size_ == 1){
+                delete tail;
+                tail = nullptr;
+                head = nullptr;
+                size_--;
+
+                deleted = true;
+            }else{
+                tail = tail->prev;
+                delete tail->next;
+                tail->next = nullptr;
+                size_--;
+
+                deleted = true;
+            }
+
+            return deleted;
+        }
+
+        bool pop_front(){
+            bool deleted = false;
+
+            if(size_ == 0){
+                throw std::runtime_error("Error trying to pop_front -- The list is empty");
+            }else if(size_ == 1){
+                delete head;
+                tail = nullptr;
+                head = nullptr;
+                size_--;
+
+                deleted = true;
+            }else{
+                head = head->next;
+                delete head->prev;
+                head->prev = nullptr;
+                size_--;
+
+                deleted = true;
+            }
+
+            return deleted;
+        }
+
+        bool remove_first(int value){
+            bool removed = false;
+            int value_index = find_first(value);
+
+            if(value_index == 0){
+                removed = pop_front();
+            }else if(value_index == (int) (size_ - 1)){
+                removed = pop_back();
+            }else{
+                int_node* value_addr;
+                // check if index is closer to the start or the end
+                int half_size = ((size_ - 1) / 2);
+                if(value_index <= half_size){
+                    int_node* iter = head;
+                    // loop to get the value address
+                    for(unsigned int i = 0; i < size_; i++){
+                        if((int) i == value_index){
+                            value_addr = iter;
+                            break;
+                        }
+
+                        iter = iter->next;
+                    }
+                }else{
+                    int_node* iter = tail;
+                    // loop to get the value address
+                    for(unsigned int i = (size_ - 1); i > 0; i--){
+                        if((int) i == value_index){
+                            value_addr = iter;
+                            break;
+                        }
+
+                        iter = iter->prev;
+                    }
+                }
+
+                // actually remove the value
+
+                int_node* before_value = value_addr->prev;
+                int_node* after_value = value_addr->next;
+
+                // changes the pointers before and after value address. So, the value address is not anymore linked to the linked list
+                before_value->next = after_value;
+                after_value->prev = before_value;
+
+                delete value_addr;
+
+                removed = true;
+            }
+
+            if(removed){
+                size_--;
+            }
+
+            return removed;
+        }
+
+
+        bool remove_at(unsigned int index){
+            bool removed = false;
+
+            if(index == 0){
+                removed = pop_front();
+            }else if(index == (size_ - 1)){
+                removed = pop_back();
+            }else{
+                // Loops to get the index memory address
+
+                int_node* index_addr;
+
+                // check if the index is closer to start or the end
+                unsigned int half_size = (size_ - 1) / 2;
+                if(index <= half_size){
+                    int_node* iter = head;
+                    for(unsigned int i = 0; i < size_; i++){
+                        if(i == index){
+                            index_addr = iter;
+                            break;
+                        }
+
+                        iter = iter->next;
+                    }
+                }else{
+                    int_node* iter = tail;
+                    for(unsigned int i = (size_ - 1); i > 0; i--){
+                        if(i == index){
+                            index_addr = iter;
+                        }
+
+                        iter = iter->prev;
+                    }
+                }
+
+                // actually remove the index value
+
+                int_node* before_index = index_addr->prev;
+                int_node* after_index = index_addr->next;
+
+                // unlink the index address from linked list
+                before_index->next = after_index;
+                after_index->prev = before_index;
+                delete index_addr;
+
+                removed = true;
+            }
+
+            if(removed){
+                size_--;
+            }
+
+            return removed;
         }
 };
 
